884d104a1ddb1ec4dabd68339a82df0d6c8614e4
MLAARIF <marwan.laarif94@gmail.com>
2023-08-22 03:45:10 +0100

    fixed Merge two sorted lists

--- src/main/java/com/marouane/leet/code/MergeTwoSortedLists.java
+++ src/main/java/com/marouane/leet/code/MergeTwoSortedLists.java
@@ -3,7 +3,25 @@ package com.marouane.leet.code;
 public class MergeTwoSortedLists {
 
     public static ListNode merge(ListNode list1, ListNode list2) {
-        return ListNode.of(1,5);
+        var result = new ListNode();
+        var lastPointer = result;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                lastPointer.next = list1;
+                list1 = list1.next;
+            } else {
+                lastPointer.next = list2;
+                list2 = list2.next;
+            }
+            lastPointer = lastPointer.next;
+        }
+
+        if (list1 == null) {
+            lastPointer.next = list2;
+        } else {
+            lastPointer.next = list1;
+        }
+        return result.next;
     }
 
     public static class ListNode {
@@ -27,35 +45,43 @@ public class MergeTwoSortedLists {
             var s = "[";
             var node = this;
             while (node != null) {
-                s += node.val + ", ";
+                s += node.val;
+                if (node.next != null)
+                    s += ", ";
                 node = node.next;
             }
-            return s+"]";
+            return s + "]";
         }
 
         @Override
         public boolean equals(Object obj) {
+
             if (this == obj)
                 return true;
-            if (obj instanceof ListNode other) {
-                if (this.next == null) {
-                    return this.val == other.val;
-                }else{
-                    return this.val == other.val && this.next.equals(other.next);
+            if (obj instanceof ListNode node2) {
+                var node1 = this;
+
+                while (node1 != null && node2 != null) {
+                    if (node1.val != node2.val)
+                        return false;
+                    node1 = node1.next;
+                    node2 = node2.next;
                 }
+                return true;
             }
             return false;
         }
 
         static ListNode of(int... elements) {
+            if (elements.length == 0)
+                return new ListNode(0);
             ListNode preHead = new ListNode();
             ListNode last = preHead;
             for (int i : elements) {
-                last.val = i;
-                last.next = new ListNode();
+                last.next = new ListNode(i);
                 last = last.next;
             }
-            return preHead.next ;
+            return preHead.next;
         }
     }
 }
--- src/test/java/com/marouane/leet/code/MergeTwoSortedListsTest.java
+++ src/test/java/com/marouane/leet/code/MergeTwoSortedListsTest.java
@@ -1,19 +1,31 @@
 package com.marouane.leet.code;
 
-import org.junit.jupiter.api.Test;
+import com.marouane.leet.code.MergeTwoSortedLists.ListNode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class MergeTwoSortedListsTest {
 
-    @Test
-    void merge() {
-        var l1 = MergeTwoSortedLists.ListNode.of(1,2,3);
-        System.out.println(l1);
-        var l2 = MergeTwoSortedLists.ListNode.of(3,4,5);
-        var expected = MergeTwoSortedLists.ListNode.of(1,2,3,3,4,5);
-        assertThat(MergeTwoSortedLists.merge(l1,l2)).isEqualTo(expected);
-
+    @ParameterizedTest
+    @MethodSource("lists")
+    void testMerge(ListNode l1, ListNode l2, ListNode expected) {
+        var actual = MergeTwoSortedLists.merge(l1, l2);
+        assertThat(actual).isEqualTo(expected);
     }
+
+    private static Stream<Arguments> lists() {
+        return Stream.of(
+                arguments(ListNode.of(1, 2, 3), ListNode.of(3, 4, 5), ListNode.of(1, 2, 3, 3, 4, 5)),
+                arguments(new ListNode(0), new ListNode(), ListNode.of(0)),
+                arguments(ListNode.of(1, 2, 4), ListNode.of(1, 3, 4), ListNode.of(1, 1, 2, 3, 4, 4))
+        );
+    }
+
+
 }
